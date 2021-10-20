31e146da7c4f7225dc9f01a001931c635815545b
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2021-10-20 16:54:57 +0200

    Class after T1

--- app/src/main/java/de/hawlandshut/pluto22_gwk/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto22_gwk/MainActivity.java
@@ -1,14 +1,54 @@
 package de.hawlandshut.pluto22_gwk;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "xx MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "called onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "called onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"called onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"called onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"called onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"called onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"called onDestroy");
     }
 }
